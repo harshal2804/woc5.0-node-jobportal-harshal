5a4a268326e704f1c1fb7cd42517d2352ca8464c
harshal2804 <harshal5820@gmail.com>
2023-02-09 19:11:20 +0530

    Remove deprecation warning option

--- Server/server.js
+++ Server/server.js
@@ -20,11 +20,7 @@ app.use(cors())
 app.use('/Student/auth', authRoute)
 
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-});
+mongoose.connect(process.env.MONGO_URI);
 
 var db = mongoose.connection;
 
